f3bf38604edf5006a1627523f68830134a330acf
Sollb77 <qsiecuadorservicios@gmail.com>
2023-02-07 16:41:53 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
-//import React from 'react';
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
+
 
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
@@ -16,7 +16,7 @@ const Home = () => {
 		setToDoes(tempToDo);
 		console.log(tempToDo);
 		setInputValue("");
-		fetch("http://assets.breatheco.de/apis/fake/todos/user/sollb", {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/sollb", {
 			method: "PUT",
 			headers: {
 				"Content-Type": "application/json",
@@ -38,6 +38,59 @@ const Home = () => {
 				console.log(error);
 			});
 	};
+    useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/sollb", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log("STATUS");
+				console.log(resp.status);
+				return resp.json();
+			})
+			.then((data) => {
+				console.log("console de la data");
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+				setToDoes(data);
+			})
+			.catch((error) => {
+				//manejo de errores
+				console.log(error);
+			});
+	     }, []);
+    
+		 const eliminarToDo = (indice) => {
+			let tempListRemove = toDoes.filter((item, i) => i!== indice); 
+			console.log(tempListRemove);
+			setToDoes(tempListRemove);
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/sollb", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(tempListRemove),
+		})
+			.then((resp) => {
+				console.log("STATUS");
+				console.log(resp.status);
+				return resp.json();
+			})
+			.then((data) => {
+				console.log("console de la data");
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch((error) => {
+				//manejo de errores
+				console.log(error);
+			});
+		 }
+
+
+
 
 	return (
 		<div className="container">
@@ -53,25 +106,20 @@ const Home = () => {
 			     // onKeyPress ={(e) =>  {
 				//  if (e.key === "Enter") {}
 				 // }}
-				placeholder= 'What do yo need to do?'
+				placeholder= "What do yo need to do?"
 			></input>
 				</form>
 					
 
 			</li>
 			
-			{toDoes.map((t,index) => (
+			{toDoes.map((elemento,index) => (
 			<li className='cosas p-2'>
-            {t}{" "}
+            {elemento.label}{" "}
                     
 				<i class="fas fa-trash-alt p-2"
 				onClick={() =>
-				setToDoes(
-					toDoes.filter(
-						(t,currentIndex) =>
-						index != currentIndex
-					)
-				)
+				eliminarToDo(index)
 				 }></i>
 				</li>
 
